4f06e22926e6971c9e39aa9ef1718207bc912592
Andrey0295 <andreevicandrej00@gmail.com>
2021-08-19 13:31:29 +0300

    adds tests for unauthorized user

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -35,9 +35,6 @@ RSpec.describe "Articles", type: :request do
 
 
 
-   
-    
-
   describe 'ARTICLES CRUD' do
     before do
       post @sign_in_url, params: @login_params, as: :json
@@ -51,40 +48,52 @@ RSpec.describe "Articles", type: :request do
             get '/me', headers: @headers
             expect(response).to have_http_status(200)
           end
-    it 'GET returns all articles' do
+    it 'GET /articles returns all articles' do
             get '/articles', headers: @headers
             expect(response).to have_http_status(200)
           end
-    it 'GET returns articles by an authorized user' do
+    it 'GET /my-articles returns articles by an authorized user' do
             get '/my-articles', headers: @headers
             expect(response).to have_http_status(200)
           end
-    it 'POST create a new article, if user authorized, returns status 200' do
+    it 'POST /articles create a new article, if user authorized, returns status 200' do
             post '/articles', params: @article_params, headers: @headers
             expect(response).to have_http_status(200)
           end 
-    it 'PUT edit an article, returns status 200' do
+    it 'PUT /articles/article_id edits an article, returns status 200' do
             put "/articles/#{@article.id}", params: @edit_article_params, headers: @headers
             expect(response).to have_http_status(200)
           end 
-    it 'DELETE an article, returns 204' do
+    it 'DELETE /articles/aricle_id an article, returns 204' do
             delete "/articles/#{@article.id}", headers: @headers
             expect(response).to have_http_status(204)     
-          end       
+          end 
+          
+          
     context 'when login params is invalid' do
             before { post @sign_in_url }
-    it 'POST create a new article, if user unauthorized, returns status 401 ' do
+    it 'GET /articles returns status 401' do
+            get '/articles'
+            expect(response).to have_http_status(401)
+          end  
+    it 'GET /my-articles returns status 401' do
+            get '/my-articles'
+            expect(response).to have_http_status(401)
+          end             
+    it 'POST create a new article returns status 401' do
             post '/articles', params: @article_params
             expect(response).to have_http_status(401)
           end
-    it 'PUT edit an articles, if user unauthorized, returns status 401' do
+    it 'PUT /articles/article_id edit an articles returns status 401' do
             delete "/articles/#{@article.id}", params: @edit_article_params
             expect(response).to have_http_status(401) 
           end
-    it 'DELETE delete an article, if user anauthorized, returns 401' do
+    it 'DELETE /articles/article_id delete an article returns 401' do
             delete "/articles/#{@article.id}"
             expect(response).to have_http_status(401)     
           end       
         end          
   end
+
+
 end
